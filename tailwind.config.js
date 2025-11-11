52eeae2a5b16e993b5dff5c354e37b2ffb1ab483
Oniye-cloud <Oniyeahmad@gmail.com>
2025-11-11 11:26:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: "class",   // <- important
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
